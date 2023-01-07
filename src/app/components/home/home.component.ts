423806f73ce05f862d8fedecf0bbb673f8f8c07a
Janatbek Sharsheyev <janatbek@gmail.com>
2023-01-07 03:14:23 -0700

    adds prettier, and seo

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { Banner } from 'src/app/models/banner.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   banner: Banner;
-  constructor() {
-    this.banner = new Banner(
-      'ca-pub-4402260762998050',
-      8896926206,
-      'auto',
-      true
-    )
+  constructor(private meta: Meta, private title: Title) {
+    this.meta.addTags([
+      { name: 'description', content: 'Home page of Janatbek Orozaly portfolio app' },
+      { name: 'author', content: 'Janatbek @ TechDevApps' },
+      { name: 'keywords', content: 'Angular in github page, Angular 15, Bootstrap, Software Engineer' },
+    ]);
+    this.banner = new Banner('ca-pub-4402260762998050', 8896926206, 'auto', true);
+  }
+
+  setTitle(): void {
+    this.title.setTitle('Janatbek Orozaly');
   }
 }
